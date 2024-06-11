d7dbac1f681d33d4157d9360ac040486fa32b74d
javiruiz1998 <fjrh98@gmail.com>
2024-06-11 16:40:46 +0200

    tipo parametros nuevo pedido

--- frontend/src/app/pages/formulario-pedido/formulario-pedido.component.ts
+++ frontend/src/app/pages/formulario-pedido/formulario-pedido.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
+import { parametrosPedido } from 'src/app/interfaces/formulario-pedido';
+
+import { FormularioNuevoPedidoService } from 'src/app/service/formulario-nuevo-pedido.service';
+
 @Component({
   selector: 'app-formulario-pedido',
   templateUrl: './formulario-pedido.component.html',
   styleUrls: ['./formulario-pedido.component.css'],
 })
 export class FormularioPedidoComponent implements OnInit {
+
   datos_pedido!: FormGroup;
+  parametros_pedido: parametrosPedido = {} as parametrosPedido;
   isButtonDisabled: boolean = true;
 
-  constructor() {}
+  constructor(private FormularioNuevoPedidoService: FormularioNuevoPedidoService) {}
 
   ngOnInit(): void {
     this.inicializarFormulario();
@@ -60,6 +66,34 @@ export class FormularioPedidoComponent implements OnInit {
 
   enviarPedido() {
     console.log('datos', this.datos_pedido.value);
-    this.inicializarFormulario();
+    this.asignarPedido();
+    this.FormularioNuevoPedidoService.newPedido(this.parametros_pedido).subscribe((response: boolean) => {
+      if (response) {
+        console.log('enviado');
+        this.datos_pedido.reset();
+      }
+    })
+  }
+
+  asignarPedido(){
+    this.parametros_pedido.nombre = this.datos_pedido.get('nombre')?.value;
+    this.parametros_pedido.apellido_1 = this.datos_pedido.get('apellido_1')?.value;
+    this.parametros_pedido.apellido_2 = this.datos_pedido.get('apellido_2')?.value;
+    this.parametros_pedido.nif = this.datos_pedido.get('nif')?.value;
+    this.parametros_pedido.email = this.datos_pedido.get('email')?.value;
+    this.parametros_pedido.telefono_1 = this.datos_pedido.get('telefono_1')?.value;
+    this.parametros_pedido.telefono_2 = this.datos_pedido.get('telefono_2')?.value;
+    this.parametros_pedido.direccion = this.datos_pedido.get('direccion')?.value;
+    this.parametros_pedido.localidad = this.datos_pedido.get('localidad')?.value;
+    this.parametros_pedido.provincia = this.datos_pedido.get('provincia')?.value;
+    this.parametros_pedido.codpos = this.datos_pedido.get('codpos')?.value;
+    this.parametros_pedido.canal = this.datos_pedido.get('canal')?.value;
+    this.parametros_pedido.id_agente = this.datos_pedido.get('id_agente')?.value;
+    this.parametros_pedido.id_tienda = this.datos_pedido.get('id_tienda')?.value;
+    this.parametros_pedido.telefono_tienda = this.datos_pedido.get('telefono_tienda')?.value;
+    this.parametros_pedido.email_tienda = this.datos_pedido.get('email_tienda')?.value;
+    this.parametros_pedido.tipo_instalacion = this.datos_pedido.get('tipo_instalacion')?.value;
+    this.parametros_pedido.oferta = this.datos_pedido.get('oferta')?.value;
+    this.parametros_pedido.comentarios = this.datos_pedido.get('comentarios')?.value;
   }
 }
--- frontend/src/app/service/formulario-nuevo-pedido.service.ts
+++ frontend/src/app/service/formulario-nuevo-pedido.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { parametrosPedido } from '../interfaces/formulario-pedido';
 @Injectable({
   providedIn: 'root'
 })
 export class FormularioNuevoPedidoService {
 
-  constructor() { }
+  private url = "http://localhost:7250/api/";
+
+  constructor(private http: HttpClient) {}
+
+  newPedido(parametros_pedido: parametrosPedido): Observable<boolean> {
+    return this.http.post<boolean>(this.url + 'new-pedido', parametros_pedido);
+  }
 }
